04d7cf241c61bd9e8f107404290f8cdc67806f19
khelpix <60488735+khelpix@users.noreply.github.com>
2021-06-23 13:01:12 -0600

    App.js updated
    I think this is cleaner.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import DashboardLayout from './layouts/DashboardLayout';
 import './vibe/scss/styles.scss';
 
-export default function App() {
-  return (
-    <BrowserRouter>
+const _BrowserRouter = () => {
+  return(<BrowserRouter>
       <Switch>
         <Route component={DashboardLayout} />
       </Switch>
-    </BrowserRouter>
-  );
+    </BrowserRouter>)
+};
+
+export default function App() {
+  return _BrowserRouter;
 }
